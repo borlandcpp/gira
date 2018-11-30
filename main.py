52cf5a96c4a0eb7d98ee224b42ef6fd2e46c4f22
Wenzhi Liang <wenzhi.liang@gmail.com>
2018-11-30 23:47:27 +0800

    ...

--- main.py
+++ main.py
@@ -3,27 +3,70 @@
 import requests
 import urllib
 import os
+import sys
+import json
 from git import Repo
-from pprint import pprint
 import giturlparse
 
 
+class GiteeError(Exception):
+    pass
+
 
 class Gitee(object):
+    api_root = "https://gitee.com/api/v5/repos/%s/%s"
+
     def __init__(self, user, token):
         self.user = user
         self.token = token
         self.git = Git()
-        self._root = "https://gitee.com/api/v5/repos/%s/%s" % (
-            self.git.info()
-        )
+        self.owner, self.repo = self.git.info()
+        self._root = Gitee.api_root % (self.owner, self.repo)
 
     def _url(self, urls, params):
-        params['access_token'] = self.token
-        return os.path.join(self._root, *urls) + '?' + urllib.parse.urlencode(params)
+        if params is not None:  # this is for GET
+            params['access_token'] = self.token
+            return os.path.join(self._root, *urls) + '?' + urllib.parse.urlencode(params)
+        else:  # for PUT and POST
+            return os.path.join(self._root, *urls)
 
     def get(self, url, params):
         return requests.get(self._url(url, params))
+
+    def get_pr(self, pr):
+        res = self.get(("pulls", pr), {})
+        if not res.status_code == 200:
+            raise GiteeError("RES %d" % res.status_code)
+        return res.text
+
+    def merge(self, pr):
+        res = requests.put(
+            self._url(("pulls", pr, "merge"), None),
+            data = {
+                "access_token": self.token,
+                "owner": self.owner,
+                "repo": self.repo,
+                "number": pr
+            }
+        )
+        if not res.status_code == 200:
+            raise GiteeError(res.text)
+
+
+class PR(object):
+    def __init__(self, jsn):
+        # TODO: handle exceptions
+        self.raw = jsn
+        self.data = json.loads(jsn)
+
+    def good(self):
+        return self.data["assignee"] and self.data["tester"]
+
+    def merged(self):
+        return self.data["state"] == "merged"
+
+    def dump(self):
+        print(self.raw)
 
 
 class Git(object):
@@ -39,6 +82,35 @@ class Git(object):
         return p.owner, p.repo
 
 
-gitee = Gitee("borlandc", "efc230e91b187fdf8021018fc76a575c")
-pr = gitee.get(("pulls", "1"), {})
-print(pr.text)
+def main(user, token, no):
+    try:
+        gitee = Gitee(user, token)
+        pr = PR(gitee.get_pr(no))
+        if not pr.good():
+            print("Invalid PR. Should be assigned to reviwer as well as tester")
+            return 0
+        elif pr.merged():
+            print("Already merged. Nothing to do")
+            return 0
+    except GiteeError as e:
+        print("Error: %s" % e, file=sys.stderr)
+        return 1
+
+    try:
+        gitee.merge(no)
+    except GiteeError as e:
+        pr.dump()
+        print("\n\nFailed to merge PR: %s" % e, file=sys.stderr)
+        sys.exit(2)
+
+
+def must_env(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        print("Missing environment variable: %s" % name)
+        sys.exit(3)
+
+if __name__ == "__main__":
+    pr = sys.argv[1]
+    sys.exit(main(must_env("GITEE_USER"), must_env("GITEE_TOKEN"), pr))
